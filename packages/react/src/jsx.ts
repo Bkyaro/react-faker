2ea9b9f6d803618f4e32d23aeb9322b8eb07cace
Jackal_lzt <bkyarokun@gmail.com>
2024-07-05 17:00:33 +0800

    feat: 实现 jsx方法 jsxDEV方法 React.createElement方法

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,69 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import {
+	Type,
+	Key,
+	Ref,
+	Props,
+	ReactElement,
+	ElementType
+} from 'shared/ReactTypes';
+// ReactElement
+const ReactElement = function (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElement {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		key,
+		type,
+		ref,
+		props,
+		__mark: 'Faker'
+	};
+	return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		// config 中的 key 和 ref 单独处理
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+
+		// 检查是否是config自己身上的属性
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	// 判断是否有其他参数
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+	return ReactElement(type, key, ref, props);
+};
+
+// 生产环境和开发环境的jsx用同样实现(实际上 react 源码中 jsxDEV 用的是不同实现)
+export const jsxDEV = jsx;
