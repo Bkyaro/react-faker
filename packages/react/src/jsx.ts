ecd000f5523b6cb7dab33f9b55e07bfa17d3470a
Jackal_lzt <bkyarokun@gmail.com>
2024-07-09 15:49:57 +0800

    feat: 修改jsxDev方法

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -65,5 +65,33 @@ export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
 	return ReactElement(type, key, ref, props);
 };
 
-// 生产环境和开发环境的jsx用同样实现(实际上 react 源码中 jsxDEV 用的是不同实现)
-export const jsxDEV = jsx;
+export const jsxDev = (type: ElementType, config: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		// config 中的 key 和 ref 单独处理
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+
+		// 检查是否是config自己身上的属性
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
